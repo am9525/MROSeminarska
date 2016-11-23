aec199be1c2ec33bf40a00998dfc99523d5b5fbc
Škatla <Aljaz@192.168.145.128>
2016-11-23 12:41:32 +0100

    finished implementing queue

--- Seminarska1/Router.cc
+++ Seminarska1/Router.cc
@@ -30,9 +30,11 @@ private:
     int processing;
     int resources;
     int lostPackets;
+    bool firstMsg;
 protected:
     virtual void initialize() override;
     virtual void handleMessage(cMessage * msg) override;
+    virtual void updateDisplay(int i, int p, int r, int l);
 };
 Define_Module(Router);
 Router::Router(){
@@ -44,6 +46,7 @@ Router::Router(){
     gateTable.clear();
     msgQueue.clear();
     systemMsg = nullptr;
+    firstMsg = true;
 }
 Router::~Router(){
 
@@ -87,39 +90,12 @@ if(msg == systemMsg){
 }*/
 void Router::handleMessage(cMessage * msg){
     IP_Message* packet = check_and_cast<IP_Message* >(msg);
-    /*
-    EV << queueCapacity << endl;
-    if(queueCapacity == 0){
-        EV << "losing message queue is full" << endl;
-        lostPackets++;
-        delete(msg);
-    }
-    else{
-        //if queue is empty there is  not need to put it in the queue can go directly into processing
-        if(msgQueue.empty()){
-            packet = check_and_cast<IP_Message* >(msg);
-        }
-        else{
-            EV << "pushing message on the queue" << endl;
-            //push the message on the queue
-            msgQueue.insert(msg);
-            queueCapacity--;
-            //take the first message from the queue
-            packet = check_and_cast<IP_Message* >(msgQueue.front());
-            msgQueue.pop();
-        }
-    }*/
+
     //--------------------------------------------System messages that have priority--------------------------------------
+    //this messages dont go into que
     //send initial message, this is sent from source
     if(strcmp("job", packet->getName()) == 0){
-        //print routing table
-        /*
-        EV << getName() << " routing table" << endl;
-        for(int i = 0; i < addTable.size(); i++){
-            EV << addTable[i] << " gate:" << gateTable[i] << endl;
-
-        }*/
-        //select random raouter to send the message to
+        //select random router to send the message to
         int selectRouter = rand()%addTable.size();
         EV<< "index of selected router :" << selectRouter << endl;
         //create new msg
@@ -134,8 +110,8 @@ void Router::handleMessage(cMessage * msg){
         delete(packet);
     }
 
-
-    //every router sends his address to neighbouring routers
+    //if we get the startBroadcast systemMSG
+    //every router sends his address to neighboring routers
     else if(strcmp(packet->getName(),"startBroadcast") == 0){
         EV << getName() << " is starting broadcast" << endl;
         bubble("Broadcasting my own address");
@@ -187,23 +163,16 @@ void Router::handleMessage(cMessage * msg){
             delete(msg);
             delete(packet);
         }
-    }else{
-        if(queueCapacity == 0){
-            EV << "losing message queue is full" << endl;
-            lostPackets++;
-            delete(msg);
-        }
-        else{
-            msgQueue.insert(msg);
-            queueCapacity--;
-        }
     }
-    //-----------------------------------------------------------------------------------------------------------
-    //if we have enough resources and the previous message has finished processing
-    if(processing < resources && strcmp("finishedProcessing", packet->getName()) == 0){
-        //we are processing one more message
-        processing++;
-
+    //if we get the finished processing systeMsg
+    else if(strcmp("finishedProcessing", packet->getName()) == 0 && !msgQueue.isEmpty()){
+        packet = check_and_cast<IP_Message* >(msgQueue.front());
+        msgQueue.pop();
+        queueCapacity++;
+        processing--;
+        EV << "processing finished" << endl;
+        bubble("processing finished");
+        //if we get a person message
         if(strcmp("personalMessage", packet->getName()) == 0){
             //packet arrived at destination
             if(strcmp(packet->getIP_destination(), ipAddress.c_str()) == 0){
@@ -248,13 +217,12 @@ void Router::handleMessage(cMessage * msg){
                     //delete(packet);
                 }
             }
-
         }
         if(strcmp("Acknowledgment", packet->getName()) == 0){
             //packet arrived ad destination
             if(strcmp(packet->getIP_destination(), ipAddress.c_str()) == 0){
-                bubble("recieved acknowledgment");
-                EV << "recieved acknowledgment from: " << packet->getIP_source() << endl;
+                bubble("Received acknowledgment");
+                EV << "Received acknowledgment from: " << packet->getIP_source() << endl;
                 send(packet, "sinkConn");
                 //delete(msg);
             }
@@ -280,13 +248,39 @@ void Router::handleMessage(cMessage * msg){
                     delete(packet);
                 }
             }
+
         }
+    }
+    //if we get a "personalMessage" or "Acknowledgment" put it in queue
+    else{
+        if(queueCapacity == 0){
+            EV << "losing message queue is full" << endl;
+            lostPackets++;
+            delete(msg);
+        }
+        else{
+            msgQueue.insert(msg);
+            queueCapacity--;
+        }
+    }
+    //-----------------------------------------------------------------------------------------------------------
+    //if we have enough resources we can process the message
+    if(processing < resources && !msgQueue.isEmpty()){
+        processing++;
+        //we are processing one more message
         systemMsg = new IP_Message("finishedProcessing");
         systemMsg->setIP_source("");
         systemMsg->setIP_destination("");
         systemMsg->setContent("");
-        //vsak router doda svoje sosede
-        scheduleAt(simTime()+5.0,systemMsg);
+        EV << "processing..." << endl;
+        bubble("processing message");
+        scheduleAt(simTime()+par("processingTime"),systemMsg);
     }
+    updateDisplay(queueCapacity, resources, processing,lostPackets);
 }
-
+void Router::updateDisplay(int i, int p, int r, int l)
+{
+    char buf[30];
+    sprintf(buf, "Q_l :%ld, R: %ld/%ld Lp: %ld", (long) i, (long) p, (long) r, long(l));
+    getDisplayString().setTagArg("t",0,buf);
+}
